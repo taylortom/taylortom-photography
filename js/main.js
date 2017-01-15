082376df1d73695956ed1d6ef0905cdd0ef8e7d6
Thomas Taylor <thomas.taylor@kineo.com>
2017-01-15 23:54:49 +0000

    Add albums view, minimising header bar

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 // hide lightbox
 $('.lightbox').ready(function() { $('.lightbox').hide(); });
 
+var photosDone;
+var albumsDone;
+var profileDone;
 // START
 $(onReady);
 var photos = false;
+var albums = false;
 var userData = false;
 var page = false;
 var pageNo = 1;
@@ -11,14 +15,21 @@ var loading = false;
 
 function onReady() {
   setUpListeners();
-  loadPhotos();
-  loadProfile();
+  getContainer().css('margin-top', $('.header').outerHeight());
+  getContainer().hide();
+  showLoader();
+  loadData(function() {
+    hideLoader();
+    getContainer().show();
+    render();
+  });
   updateFooter();
 }
 
 function setUpListeners(xmlData) {
   $('.controls a[data-page=feed]').click(onFeedClicked);
   $('.controls a[data-page="wall"]').click(onWallClicked);
+  $('.controls a[data-page="albums"]').click(onAlbumsClicked);
 
   $('.lightbox img').click(closeLightBox);
   $('a.up').click(onUpClicked);
@@ -26,17 +37,33 @@ function setUpListeners(xmlData) {
   $(document).scroll(onPageScroll);
 }
 
-function loadPhotos(xmlData) {
+function loadData(dataLoaded) {
+  loadPhotos(function() {
+    photosDone = true;
+    checkLoadStatus();
+  });
+  loadAlbums(function() {
+    albumsDone = true;
+    checkLoadStatus();
+  });
+  loadProfile(function() {
+    profileDone = true;
+    checkLoadStatus();
+  });
+  function checkLoadStatus() {
+    if(photosDone && albumsDone && profileDone) dataLoaded();
+  }
+}
+
+function loadPhotos(loaded) {
   $.getJSON('https://api.flickr.com/services/rest/?method=flickr.people.getPhotos&jsoncallback=?', {
     api_key: '1be25adcdc5b396e8bc795271fb8f0d7',
     user_id: '12202482@N03',
     extras: 'date_upload,date_taken,views',
     format: 'json'
   }, function done(data, message, response) {
-    if(response.status === 200) {
-      processPhotoData(data.photos);
-      renderFeed();
-    }
+    if(response.status === 200) processPhotoData(data.photos);
+    loaded();
   });
 }
 
@@ -54,7 +81,61 @@ function processPhotoData(data) {
   });
 }
 
-function loadProfile(xmlData) {
+function loadAlbums(loaded) {
+  albums = [];
+  albumsLoaded = [];
+  var albumData;
+  var albumsToLoad = -1;
+  var index = 0;
+  $.getJSON('https://api.flickr.com/services/rest/?method=flickr.photosets.getList&jsoncallback=?', {
+    api_key: '1be25adcdc5b396e8bc795271fb8f0d7',
+    user_id: '12202482@N03',
+    per_page: 100,
+    primary_photo_extras: 'date_taken',
+    format: 'json'
+  }, function done(data, message, response) {
+    if(response.status === 200) {
+      albumsToLoad = data.photosets.photoset.length;
+      albumData = data;
+      loadAlbumData();
+    } else {
+      loaded();
+    }
+  });
+  function loadAlbumData() {
+    var album = albumData.photosets.photoset[index++];
+    $.getJSON('https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&jsoncallback=?', {
+      api_key: '1be25adcdc5b396e8bc795271fb8f0d7',
+      photoset_id: album.id,
+      user_id: '12202482@N03',
+      format: 'json'
+    }, function done(data, message, response) {
+      if(response.status === 200) {
+        albums.push({
+          id: album.id,
+          title: album.title._content,
+          description: album.description._content,
+          datetaken: album.primary_photo_extras.datetaken,
+          views: album.count_views,
+          primary: album.primary,
+          farm: album.farm,
+          server: album.server,
+          secret: album.secret,
+          photos: data.photoset.photo
+        });
+      }
+      checkLoaded(album.id);
+      if(index < albumsToLoad) loadAlbumData();
+    });
+  }
+  function checkLoaded(albumId) {
+    var index = (function() { for(var i = 0, c = albumsLoaded.length; i < c; i++) { if(albumsLoaded[i] === albumId) return i; } return -1; })();
+    if(index < 0) albumsLoaded.push(albumId);
+    if(albumsToLoad === albums.length) loaded();
+  }
+}
+
+function loadProfile(loaded) {
   $.getJSON('https://api.flickr.com/services/rest/?method=flickr.people.getInfo&jsoncallback=?', {
     api_key: '1be25adcdc5b396e8bc795271fb8f0d7',
     user_id: '12202482@N03',
@@ -73,11 +154,17 @@ function loadProfile(xmlData) {
 
       $('.header .title a').attr('href', userData.profileurl);
     }
+    loaded();
   });
 }
 
 function updateFooter() {
   $('.footer .year').html((new Date()).getFullYear());
+}
+
+function render() {
+  renderFeed();
+  $('.footer').css('position', 'initial');
 }
 
 function renderFeed(event) {
@@ -118,15 +205,60 @@ function renderWall(event) {
   getContainer().empty();
 
   var photosLoaded = 0;
+  var min = 10;
 
   for(var i = 0; i < photos.length; i++) {
     renderPhoto(photos[i], TYPE, photoWallDiv, function photoRendered(error, $el) {
-      var min = 10;
       $el.css({
-        "margin-right": getRandom(min,40),
-        "margin-bottom": getRandom(min,60)
+        "margin-right": getRandomPhotoSpacing(),
+        "margin-bottom": getRandomPhotoSpacing()
       });
       $el.css('opacity', 1);
+    });
+  }
+}
+
+function renderAlbums() {
+  var TYPE = 'albums';
+  if(page === TYPE) return;
+  page = TYPE;
+
+  var container = getContainer();
+  container.empty();
+  for(var i = 0, count = albums.length; i < count; i++) {
+    var album = albums[i];
+    var date = new Date(album.datetaken);
+    var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    var thumbUrl = getPhotoURL(album.primary, album.farm, album.server, album.secret, 'm');
+    var div = '<div class="album" data-id="' + album.id + '">';
+      div += '<div class="thumb" style="background-image: url(' + thumbUrl + ');"></div>';
+      div += '<div class="title">';
+        div += '<div class="inner">' + album.title + '</div>';
+      div += '</div>';
+      // div += '<div class="date">' + months[date.getMonth()].substr(0,3) + ' ' + date.getFullYear() + '</div>';
+    div += '</div>';
+    container.append(div);
+  }
+
+  $('.album').click(onAlbumClicked);
+}
+
+function renderAlbum(album) {
+  var TYPE = 'album';
+  if(page === TYPE) return;
+  page = TYPE;
+
+  var container = getContainer();
+  container.empty();
+
+  for(var i = 0, count = album.photos.length; i < count; i++) {
+    renderPhoto(album.photos[i], 'wall', photoWallDiv, function(error, $photo) {
+      $photo
+        .css({
+          "margin-right": getRandomPhotoSpacing(),
+          "margin-bottom": getRandomPhotoSpacing()
+        })
+        .animate({ opacity: 1 });
     });
   }
 }
@@ -134,7 +266,6 @@ function renderWall(event) {
 function renderPhoto(data, type, template, done) {
   var cached = data.cache && data.cache[type];
   getContainer().append(cached || template(data));
-
 
   var $photo = $('.photo[id=' + data.id + ']')
     // listen to events
@@ -146,9 +277,11 @@ function renderPhoto(data, type, template, done) {
     $('img', $photo).imagesLoaded()
       .always(function() {
         cachePhoto(type, $photo);
-        done(null, $photo);
+        if(done) done(null, $photo);
       });
-  } else done(null, $photo);
+  } else {
+    if(done) done(null, $photo);
+  }
 }
 
 function photoPostDiv(data) {
@@ -180,6 +313,7 @@ function photoWallDiv(data) {
 
 function cachePhoto(cacheName, $el) {
   var photoData = getPhotoById($el.attr('id'));
+  if(!photoData) return;
   if(!photoData.cache) photoData.cache = {};
   if(!photoData.cache[cacheName]) photoData.cache[cacheName] = $el;
 }
@@ -211,15 +345,28 @@ function getDataFromURL(url) {
   };
 }
 
+function getAlbumById(id) {
+  for(var i = 0, count = albums.length; i < count; i++) {
+    if(albums[i].id === id) return albums[i];
+  }
+  console.log('No album found with ID:', id);
+  return null;
+}
+
 function getPhotoById(id) {
   for(var i = 0; i < photos.length; i++) {
     if(photos[i].id === id) return photos[i];
   }
+  console.log('No photo found with ID:', id);
   return null;
 }
 
 function getRandom(min, max) {
   return Math.random() * (max - min) + min;
+}
+
+function getRandomPhotoSpacing(min, max) {
+  return getRandom(min || 15, max || 40);
 }
 
 function getContainer() {
@@ -255,8 +402,14 @@ function closeLightBox() {
 
 function onPageScroll(event) {
   var preloadScrollThreshold = ($(document).height() - window.innerHeight) - ($(document).height() * 0.10);
-  var upScrollThreshold = window.innerHeight * 1.1;
+  var upScrollThreshold = window.innerHeight * 0.5;
   var scrollPos = $(document).scrollTop();
+
+  if(scrollPos > 15) {
+    $('.header').addClass('mini');
+  } else {
+    $('.header').removeClass('mini');
+  }
 
   if(scrollPos > upScrollThreshold) {
     $('.up').fadeIn();
@@ -264,7 +417,7 @@ function onPageScroll(event) {
     $('.up').fadeOut();
   }
 
-  if(!loading && (scrollPos > preloadScrollThreshold)) {
+  if(page === 'feed' && !loading && (scrollPos > preloadScrollThreshold)) {
     renderFeedPage(pageNo+1);
   }
 }
@@ -294,6 +447,16 @@ function onFeedClicked(event) {
 function onWallClicked(event) {
   event && event.preventDefault();
   renderWall();
+}
+
+function onAlbumsClicked(event) {
+  event && event.preventDefault();
+  renderAlbums();
+}
+
+function onAlbumClicked(event) {
+  event && event.preventDefault();
+  renderAlbum(getAlbumById($(event.currentTarget).attr('data-id')));
 }
 
 function handleError() {
